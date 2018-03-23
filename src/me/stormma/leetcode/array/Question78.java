3851cd5f29b8fc23fde0efc2ca486d43393f97d8
StormMa <StormMaybin@gmail.com>
2018-03-23 15:43:08 +0800

    leetcode 78. Subsets

--- src/me/stormma/leetcode/array/Question78.java
+++ src/me/stormma/leetcode/array/Question78.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @date 2018/03/23
  */
 public class Question78 {
-    static class Solution {
+    static class Solution1 {
         public List<List<Integer>> subsets(int[] nums) {
             List<List<Integer>> ans = new ArrayList<>();
             if (nums == null || nums.length == 0) return ans;
@@ -28,8 +28,36 @@ public class Question78 {
         }
     }
 
+    /**
+     * 若nums数组有n个元素，那么结果集必定是2^n个
+     * 现在假定一个n位二进制整数   0000000000  => n位
+     * 来标识nums某个下标元素是否取, 以{1, 2, 3}举例说明
+     * 我们用一个三位二进制数来做分析:
+     * 000 => 表示2下标不取, 1下标不取, 0下标不取 => {}
+     * 001 => 表示2下标不取, 1下标不取, 0下标取 => {1}
+     * 010 => 表示2下标不取, 1下标取, 0下标不取 =>{2}
+     * .... 可得结果
+     * 111 => 7 全部取{1, 2, 3}
+     */
+    static class Solution2 {
+        public List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            if (nums == null || nums.length == 0) return ans;
+            int cnt = (int) Math.pow(2, nums.length);
+            for (int i = 0; i < cnt; i++) {
+                ans.add(new ArrayList<>());
+            }
+            for (int i = 0; i < cnt; i++) { // 2^n * n
+                for (int j = 0; j < nums.length; j++) {
+                    if (((i >> j) & 1) == 1) ans.get(i).add(nums[j]);
+                }
+            }
+            return ans;
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
-        new Solution().subsets(nums);
+        new Solution1().subsets(nums);
     }
 }
