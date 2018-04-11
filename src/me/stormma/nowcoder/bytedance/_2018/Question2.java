431ab9ebf05cf687e6561d9ea14a24f1e65235b2
StormMa <StormMaybin@gmail.com>
2018-04-11 21:38:22 +0800

    byte_dance

--- src/me/stormma/nowcoder/bytedance/_2018/Question2.java
+++ src/me/stormma/nowcoder/bytedance/_2018/Question2.java
@@ -1,49 +1,41 @@
 package me.stormma.nowcoder.bytedance._2018;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Question2 {
+    static class Solution {
+        static class Node {
+            int s;
+            int m;
+            int count;
 
-    static class Main {
-        private static final Scanner in = new Scanner(System.in);
-        public static void main(String[] args) {
-//            while (in.hasNext()) {
-                // s = "a", m = "a"
-                // 操作1: m = s, s = s + s // s = "aa" m = "aa" s = "aaaa"
-                // 操作2: s = s + m
-                String[] dpS = new String[100];
-                String[] dpM = new String[100];
-                System.out.println("sss");
-                int n = in.nextInt();
-                // m = "a", s = "aa"
-                // m = "aa", s = "aaaa"
-                // s = s + m => s = "aaaaaa";
-                dpS[1] = "aa";
-                dpM[1] = "a";
-                dpS[2] = "aaaa";
-                dpM[2] = "aa";
-                for (int i = 3; i < dpS.length; i++) {
-                    String preS = dpS[i - 1];
-                    String preM = dpM[i - 1];
-                    String tmp1M = preS;
-                    String tmp1S = preS + preS;
-                    String tmp2S = preS + preM;
-                    String tmp2M = preM;
-                    if (tmp1S.length() >= tmp2S.length()) {
-                        dpS[i] = tmp1S;
-                        dpM[i] = tmp1M;
-                    } else {
-                        dpS[i] = tmp2S;
-                        dpM[i] = tmp2M;
-                    }
-                }
-                System.out.println(dpS[99].length());
-//            }
+            public Node(int s, int m, int count) {
+                this.s = s;
+                this.m = m;
+                this.count = count;
+            }
         }
-
-//        private int solve(String s, String m, int n) {
-//
-//        }
-
+        public static void main(String... args) {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(new Node(1, 1, 0));
+            int ans = 0;
+            int cnt = 0;
+            while (!queue.isEmpty()) {
+                cnt++;
+                Node node = queue.poll();
+                if (node.s == n) {
+                    ans = node.count;
+                    break;
+                } else if (node.s < n) {
+                    Node node1 = new Node(node.s * 2, node.s, node.count + 1);
+                    Node node2 = new Node(node.s + node.m, node.m, node.count + 1);
+                    queue.add(node1);
+                    queue.add(node2);
+                }
+            }
+            System.out.println(ans + "==>" + cnt);
+        }
     }
 }
