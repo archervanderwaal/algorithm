ed1761eb865b673bf21eef005c98886f0b31876b
archer <archer.vanderwaal@gmail.com>
2018-09-04 20:49:43 +0800

    BitSet

---
+++ src/me/stormma/others/BitSetTest.java
@@ -0,0 +1,22 @@
+package me.stormma.others;
+
+import java.util.BitSet;
+
+public class BitSetTest {
+
+    public static void main(String[] args) {
+        int[] sources = {9, 2, 1, 6, 11};
+        int max = 0x80000000;
+        for (int source : sources) {
+            if (source > max) {
+                max = source;
+            }
+        }
+        BitSet bitSet = new BitSet(max + 1);
+        for (int source : sources) {
+            bitSet.set(source, true);
+        }
+        System.out.println(bitSet);
+    }
+
+}
